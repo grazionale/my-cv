43c54296a6bf63174c14e0a93421f6115da95f15
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2021-01-27 23:32:59 -0300

    Fix crossOrigin

--- components/seo/index.js
+++ components/seo/index.js
@@ -6,7 +6,7 @@ const Seo = () => {
       <title>Gabriel Grazionale</title>
       <link rel="icon" href="/favicon.ico" />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous" />
     </Head>
   )
 }
