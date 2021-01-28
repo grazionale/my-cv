79971f8fe4ebbc2002213ec100c771c6534e383c
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2021-01-27 23:32:05 -0300

    Refactor Tag Cloud

--- components/tagCloud/index.js
+++ components/tagCloud/index.js
@@ -1,16 +1,12 @@
 import styles from "./TagCloud.module.css";
 import SkillTag from "../SkillTag";
 
-const TagCloud = () => {
+const TagCloud = ({ data = [] }) => {
   return (
     <div className={styles.tagCloud}>
-      <SkillTag text="PHP" />
-      <SkillTag text="Javascript" />
-      <SkillTag text=".Net Core" />
-      <SkillTag text="MySql" />
-      <SkillTag text="Oracle" />
-      <SkillTag text="SQL Server" />
-      <SkillTag text="Ruby on Rails" />
+      {data.map(skill => (
+        <SkillTag text={skill} />
+      ))}
     </div>
   )
 }
